e671cd966813c9a83925cc211ae160a1c450985f
chiahc1 <chiahc1@uc.edu>
2014-02-08 13:26:53 -0800

    Add Html5-boilerplateSkeleton

--- blababel/helloworld.py
+++ blababel/helloworld.py
@@ -4,7 +4,8 @@ form = """<input type='submit'> What is your birthday </input>"""
 
 class MainPage(webapp2.RequestHandler):
 	def get(self):
-		self.response.headers['Content-Type'] = 'text/plain'
+		self.response.headers['Content-Type'] = 'text/html'
 		self.response.write(form)
+
 
 application = webapp2.WSGIApplication([('/', MainPage),], debug=True)
