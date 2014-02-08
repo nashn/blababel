063b1be9255f73aebc9cd923e2b920746baefbc8
gjeck <gsjeckell@gmail.com>
2014-02-08 12:37:28 -0800

    Add hello world google app engine project

---
+++ blababel/helloworld.py
@@ -0,0 +1,10 @@
+import webapp2
+
+form = """<input type='submit'> What is your birthday </input>"""
+
+class MainPage(webapp2.RequestHandler):
+	def get(self):
+		self.response.headers['Content-Type'] = 'text/plain'
+		self.response.write(form)
+
+application = webapp2.WSGIApplication([('/', MainPage),], debug=True)
